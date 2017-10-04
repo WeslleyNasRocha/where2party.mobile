25b3fc070bfc6ab6b264fff6da2ecb8ebb3f182c
Weslley Nascimento Rocha <weslleytato.12@gmail.com>
2017-10-04 13:24:56 -0300

    ScrollList => FlatList

--- src/Actions/Types/FeedTypes.js
+++ src/Actions/Types/FeedTypes.js
@@ -1,1 +1,2 @@
-export const FEED_EVENTS_FETCH_SUCCESS = 'feed_events_fetch_success';
+export const FEED_EVENTS_FETCH_SUCCESS = "feed_events_fetch_success";
+export const REFRESHING_FEED = "refreshing_feed";
--- src/Reducers/FeedReducer.js
+++ src/Reducers/FeedReducer.js
@@ -1,11 +1,20 @@
-import { FEED_EVENTS_FETCH_SUCCESS } from '../Actions/Types';
+import _ from "lodash";
+import { FEED_EVENTS_FETCH_SUCCESS, REFRESHING_FEED } from "../Actions/Types";
 
-const FeedInitialState = {};
+const FeedInitialState = {
+  events: null,
+  refreshing: false
+};
 
 export default (state = FeedInitialState, action) => {
   switch (action.type) {
     case FEED_EVENTS_FETCH_SUCCESS:
-      return action.payload;
+      const a = _.map(action.payload, (val, uid) => {
+        return { uid: uid, ...val };
+      });
+      return { ...state, events: a, refreshing: false };
+    case REFRESHING_FEED:
+      return { ...state, refreshing: true };
     default:
       return state;
   }
